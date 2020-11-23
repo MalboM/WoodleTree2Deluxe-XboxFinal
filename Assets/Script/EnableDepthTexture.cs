588f17fd454e496d6e641fee700282875ebdb8fc
MalboM <fabioferrara1989@gmail.com>
2020-11-23 17:39:05 +0100

    cc

---
+++ Assets/Script/EnableDepthTexture.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EnableDepthTexture : MonoBehaviour
+{
+    void Start()
+    {
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
